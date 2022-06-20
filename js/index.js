e890aea5c09025d08ebe89f06ed9a24b2bd10f67
kyohyunkim <86403089+kyohyunkim@users.noreply.github.com>
2022-06-20 22:44:42 +0900

    v-1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 50,
+    effect: "fade",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
 const line = document.querySelector("#line");
 const ball = document.querySelector("#ball");
 const main = document.querySelector("main");
